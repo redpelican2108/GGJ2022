de67fa563f541b4fd80705857c4baf4f4eacd121
knotstoks <70320812+knotstoks@users.noreply.github.com>
2022-01-22 02:33:14 +0800

    Add Player Animations without scripting

---
+++ GGJ/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        
+    }
+}
