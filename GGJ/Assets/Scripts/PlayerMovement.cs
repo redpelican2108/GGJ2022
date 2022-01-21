a2aa8d2d7da565de81c711401a985e799e8ace83
knotstoks <70320812+knotstoks@users.noreply.github.com>
2022-01-22 03:10:32 +0800

    Add animations to the Player

--- GGJ/Assets/Scripts/PlayerMovement.cs
+++ GGJ/Assets/Scripts/PlayerMovement.cs
@@ -8,14 +8,17 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private LayerMask platformMask;
     private Rigidbody2D rb;
     private BoxCollider2D _collider;
+    private Animator animator;
     private float gravity;
     private float jumpSpeed;
     private float xAxis;
+    private bool facingRight;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         _collider = GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();
 
         // Calculate gravity and jump speed
         gravity = -2 * jumpHeight / Mathf.Pow(jumpTime, 2);
@@ -29,7 +32,7 @@ public class PlayerMovement : MonoBehaviour
         GetMovementInputs();
 
         // Walking
-        rb.velocity = new Vector2(xAxis * walkSpeed, rb.velocity.y);
+        Move();
 
         // Jumping
         if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
@@ -37,11 +40,47 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
         }
 
+        // Animation
+        animator.SetFloat("walkSpeed", Mathf.Abs(xAxis));
+
+        if (IsGrounded())
+        {
+            animator.SetBool("jump", false);
+        }
+        else 
+        {
+            animator.SetBool("jump", true);
+        }
     }
 
     private void GetMovementInputs()
     {
         xAxis = Input.GetAxis("Horizontal");
+    }
+
+    private void Move()
+    {
+        if (xAxis > 0 && facingRight)
+        {
+            Flip();
+        }
+        else if (xAxis < 0 && !facingRight)
+        {
+            Flip();
+        }
+
+        rb.velocity = new Vector2(xAxis * walkSpeed, rb.velocity.y);
+    }
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 
     private bool IsGrounded()
