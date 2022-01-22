e9ad59c676d15545b18bda87bd4988f123aedd8a
knotstoks <70320812+knotstoks@users.noreply.github.com>
2022-01-22 12:54:15 +0800

    Finish Main Menu

--- GGJ/Assets/Scripts/Gameplay Managers/MainMenu.cs
+++ GGJ/Assets/Scripts/Gameplay Managers/MainMenu.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject levelSelectScreen;
+    public bool isLevelSelectScreenActive;
+
+    private void Start()
+    {
+        isLevelSelectScreenActive = false;
+    }
     public void PlayGame()
     {
         Debug.Log("ioeho");
     }
 
-    public void ToggleLevels()
+    public void ToggleLevelSelectScreen()
     {
+        if (!isLevelSelectScreenActive)
+        {
+            levelSelectScreen.SetActive(true);
+        }
+        else
+        {
+            levelSelectScreen.SetActive(false);
+        }
 
+        isLevelSelectScreenActive = !isLevelSelectScreenActive;
     }
 
     public void GoToLevel(int level)
